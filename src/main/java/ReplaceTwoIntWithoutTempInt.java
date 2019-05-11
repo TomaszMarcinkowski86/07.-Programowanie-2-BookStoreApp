c04b005fd5912ed3f28d4ba0fe1e72eca5afd5fe
Tomek_M <9454031marcinkowski@gmail.com>
2019-05-11 15:26:41 +0200

    pierwszy komit test

---
+++ src/main/java/ReplaceTwoIntWithoutTempInt.java
@@ -0,0 +1,16 @@
+public class ReplaceTwoIntWithoutTempInt {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+
+
+        a = a + b ;
+        b= a-b;
+        a=a-b;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
