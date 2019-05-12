b30fb78402c619e35c96338d77f876c2632973ce
Tomek_M <9454031marcinkowski@gmail.com>
2019-05-12 15:56:59 +0200

    DeleteBook2

--- src/main/java/BookStore/DeleteBook.java
+++ src/main/java/BookStore/DeleteBook.java
@@ -1,16 +1,23 @@
 package BookStore;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class DeleteBook {
 
     static void deleteBook(){
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj tytuł ksiązki do usunięcia: " + BookList.getBookList());
+        String tylkoTytul = BookList.getBookList().stream().map(book -> book.getTytul()).collect(Collectors.joining("; "));
+        System.out.println("Podaj tytuł ksiązki do usunięcia: " + tylkoTytul );
         String tytul = scanner.nextLine();
+//        if(tytul!=tylkoTytul){
+//            System.out.println("Nieprawidłowy tytuł!!");
+//            tytul = scanner.nextLine();
+//        }
         int indeks = tytul.indexOf(tytul);
         BookList.getBookList().remove(indeks);
-        
+
     }
 }
---
+++ src/main/java/BookStore/EditYear.java
@@ -0,0 +1,17 @@
+package BookStore;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class EditYear {
+
+    static void editYear(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj tytuł ksiązki do edycji roku wydania: " + BookList.getBookList().stream().map(book -> book.getTytul()).collect(Collectors.joining(", ")));
+        String tytul = scanner.nextLine();
+        int indeks = tytul.indexOf(tytul);
+        BookList.getBookList();
+
+    }
+}
